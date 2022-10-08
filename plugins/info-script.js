9b069837bcc182f4cf7efaf476a45c2ccf51573f
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-10-08 15:03:15 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -2,12 +2,12 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UCTVeRHM0v0Hqv_-O8yVkFpw*`
+let str = `*https://chat.whatsapp.com/J7VtphVsCUXCMTnbLsRurn*`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/inizenscuy",
-    title: 'Albedo-MultiDevice',
+    mediaUrl: "https://Instagram.com/yxbxrock",
+    title: 'Xyze Bot-[MD]',
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
